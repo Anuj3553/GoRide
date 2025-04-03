32ac2d53d3bdfb13493a326b6c1109057d2d952e
Anuj3553 <anujverma3553@gmail.com>
2025-04-03 12:24:00 +0530

    feat: add loading state and save trip data to Firestore in CreateTrip component

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -14,8 +14,11 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog"
 import { FcGoogle } from "react-icons/fc";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '@/service/firebaseConfig';
 
 
 export default function CreateTrip() {
@@ -23,6 +26,8 @@ export default function CreateTrip() {
 
     const [formData, setFormData] = useState({});
     const [openDialog, setOpenDialog] = useState(false);
+
+    const [loading, setLoading] = useState(false)
 
     const handleInputChange = (name, value) => {
         setFormData({
@@ -41,7 +46,6 @@ export default function CreateTrip() {
     })
 
     const OnGenrateTrip = async () => {
-
         const user = localStorage.getItem('user')
 
         if (!user) {
@@ -71,6 +75,8 @@ export default function CreateTrip() {
             return;
         }
 
+        setLoading(true);
+
         const FINAL_PROMPT = AI_PROMPT
             .replace('{location}', formData?.location?.label)
             .replace('{totalDays}', formData?.noOfDays)
@@ -78,11 +84,27 @@ export default function CreateTrip() {
             .replace('{budget}', formData?.budget)
             .replace('{totalDays}', formData?.noOfDays)
 
-        console.log(FINAL_PROMPT)
-
         const result = await chatSession.sendMessage(FINAL_PROMPT);
 
         console.log("result", result?.response?.text());
+        setLoading(false);
+        SaveAiTrip(result?.response?.text())
+    }
+
+    const SaveAiTrip = async (TripData) => {
+        setLoading(true)
+
+        const user = JSON.parse(localStorage.getItem('user'))
+        const docId = Date.now().toString()
+
+        await setDoc(doc(db, "AITrips", docId), {
+            userSelection: formData,
+            tripData: JSON.parse(TripData),
+            userEmail: user?.email,
+            id: docId,
+        });
+
+        setLoading(false)
     }
 
     const GetUserProfile = (tokenInfo) => {
@@ -170,9 +192,13 @@ export default function CreateTrip() {
 
             <div className='my-10 flex justify-end'>
                 <Button
+                    disabled={loading}
                     onClick={OnGenrateTrip}
                 >
-                    Generate Trip
+                    {loading ?
+                        <AiOutlineLoading3Quarters className='h-7 w-7 animate-spin' /> : "Generate Trip"
+                    }
+
                 </Button>
             </div>
 
@@ -191,7 +217,11 @@ export default function CreateTrip() {
                         <h2 className="font-bold text-lg mt-7">Sign In With Google</h2>
                         <div>Sign in to the App with Google authentication securely</div>
 
-                        <Button onClick={login} className="w-full mt-5 flex gap-4 items-center">
+                        <Button
+                            disabled={loading}
+                            onClick={login}
+                            className="w-full mt-5 flex gap-4 items-center"
+                        >
                             <FcGoogle className="w-7 h-7" />
                             Sign In With Google
                         </Button>
